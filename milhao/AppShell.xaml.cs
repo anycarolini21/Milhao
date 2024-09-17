a25534be947f1981141cc3a26c8157965caad75a
carolini <anycarolinif340@gmail.com>
2024-09-17 11:30:11 -0300

    primeiro passo

---
+++ milhao/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace milhao;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ questao.cs
@@ -0,0 +1,79 @@
+namespace milhao 
+{
+    public class Questao
+    {
+        public string Pergunta;
+        public string RespostaUm;
+        public string RespostaDois;
+        public string RespostaTres;
+        public string RespostaQuatro;
+        public string RespostaCinco;
+        public int RespostaCerta = 0;
+        public int Nivel;
+        private Label labelPergunta;
+        private Button buttonRespostaUm;
+        private Button buttonRespostaDois;
+        private Button buttonRespostaTres;
+        private Button buttonRespostaQuatro;
+        private Button buttonRespostaCinco;
+        public Questao ()
+        {
+
+        }
+        public void Desenhar ()
+        {
+            labelPergunta.Text = Pergunta;
+            buttonRespostaUm.Text = RespostaUm;
+            buttonRespostaUm.Text = RespostaDois;
+            buttonRespostaUm.Text = RespostaTres;
+            buttonRespostaUm.Text = RespostaQuatro;
+            buttonRespostaUm.Text = RespostaCinco;
+        }
+        public Questao (Label LP, Button bt01, Button bt02,Button bt03, Button bt04, Button bt05)
+        {
+            labelPergunta = LP;
+            buttonRespostaUm = bt01;
+            buttonRespostaUm = bt02;
+            buttonRespostaUm = bt03;
+            buttonRespostaUm = bt04;
+            buttonRespostaUm = bt05;
+        }
+        public void ConfigurarDesenho (Label LP, Button bt01, Button bt02,Button bt03, Button bt04, Button bt05)
+        {
+            labelPergunta = LP;
+            buttonRespostaUm.Text = RespostaUm;
+            buttonRespostaUm.Text = RespostaDois;
+            buttonRespostaUm.Text = RespostaTres;
+            buttonRespostaUm.Text = RespostaQuatro;
+            buttonRespostaUm.Text = RespostaCinco;
+        }
+        public bool VerificarResposta (int RespostaEscolhida)
+        {
+            if ( RespostaEscolhida == RespostaCerta )
+            {
+                var b = Qual (RespostaEscolhida);
+                bool.BackgroundColor = Color.Green;
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        private Button Qual (int RespostaEscolhida)
+        {
+            if (RespostaEscolhida == 1 )
+            return buttonRespostaUm;
+            else if (RespostaEscolhida == 2 )
+            return buttonRespostaDois;
+            else if (RespostaEscolhida == 3 )
+            return buttonRespostaTres;
+            else if (RespostaEscolhida == 4 )
+            return buttonRespostaQuatro;
+            else if (RespostaEscolhida == 5 )
+            return buttonRespostaCinco;
+        }
+    
+        }
+
+    }
