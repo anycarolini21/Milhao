b4034f6c4064c95ceb137a6e3f0a0b18b8c6197c
carol <anycarolinif340@gmail.com>
2024-10-07 09:47:56 -0300

    concertando erros

--- milhao/JogoPage.xaml.cs
+++ milhao/JogoPage.xaml.cs
@@ -12,25 +12,50 @@ public partial class MainPage : ContentPage
 	    gerenciador= new Gerenciador (LabelPergunta, ButtonResposta1, ButtonResposta2, ButtonResposta3, ButtonResposta4, ButtonResposta5, Pontuacao, Nivel);
 	}
 
-	  void Button1(object sender, EventArgs args)
+	  void Botao1(object sender, EventArgs args)
 	{
 	    gerenciador.VerificaCorreta(1);
 	}
-	void Button2(object sender, EventArgs args)
+	void Botao2(object sender, EventArgs args)
 	{
 		gerenciador.VerificaCorreta(2);
 	}
-	void Button3(object sender, EventArgs args)
+	void Botao3(object sender, EventArgs args)
 	{
 		gerenciador.VerificaCorreta(3);
 	}
-	void Button4(object sender, EventArgs args)
+	void Botao4(object sender, EventArgs args)
 	{
 		gerenciador.VerificaCorreta(4);
 	}
-	void Button5(object sender, EventArgs args)
+	void Botao5(object sender, EventArgs args)
 	{
 		gerenciador.VerificaCorreta(5);
 	}
+
+	void retiraaoclicar(object sender, EventArgs args)
+	{
+		var ajuda = new RetiraErrada();
+		ajuda.ConfiguraDesenho( ButtonResposta1, ButtonResposta2,  ButtonResposta3,  ButtonResposta4, ButtonResposta5);
+		ajuda.RealizaAjuda(gerenciador.GetQuestaoCorrente());
+		(sender as Button).IsVisible = false;
+
+	}
+
+	void botaouniversitario(object sender, EventArgs args)
+	{
+		var ajuda = new Universitario();
+		ajuda.ConfiguraDesenho( ButtonResposta1, ButtonResposta2,  ButtonResposta3,  ButtonResposta4, ButtonResposta5);
+		ajuda.RealizaAjuda(gerenciador.GetQuestaoCorrente());
+		(sender as Button).IsVisible = false;
+
+	}
+
+	void botaopular(object sender, EventArgs args)
+	{
+		gerenciador.ProximaQuestao();
+		(sender as Button).IsVisible = false;
+	}
+
 
 }
